21e8d51bbe519a3694877a7d07e7d34c5371f7d9
LinaFS <paufusa40.pf@gmail.com>
2025-11-14 14:55:17 -0600

    Actualización problemas sesiones

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    if (!isset($_SESSION["usuario"])&&!isset($permiso_id["2"])){
+    if (isset($_SESSION["usuario"])&&!isset($permiso_id["1"])){
         session_destroy();
     }
 ?>
