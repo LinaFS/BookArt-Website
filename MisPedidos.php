741bc192e71de8270bbe9ac489652bb5b296e0b5
LinaFS <paufusa40.pf@gmail.com>
2026-05-20 18:39:22 -0600

    Corrección pedidos precio

--- MisPedidos.php
+++ MisPedidos.php
@@ -44,8 +44,9 @@
                             WHEN tp.id_tipoPedido = 2 THEN 'Libreta Personalizada'
                         END as nombre,
                         CASE 
+                            CASE 
                             WHEN tp.id_tipoPedido = 1 THEN c.precio
-                            WHEN tp.id_tipoPedido = 2 THEN 0
+                            WHEN tp.id_tipoPedido = 2 THEN COALESCE(per.precio, 0)
                         END as precio,
                         CASE 
                             WHEN tp.id_tipoPedido = 1 THEN c.img
